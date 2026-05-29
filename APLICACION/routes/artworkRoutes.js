df3f3d60806a5172452ad279cbe7018cd644b6c6
Laura Ortiz <131725116+Luciaortz@users.noreply.github.com>
2026-05-29 00:50:02 -0500

    se cambio la ruta

--- APLICACION/routes/artworkRoutes.js
+++ APLICACION/routes/artworkRoutes.js
@@ -13,13 +13,20 @@ router.get("/test", (req,res)=>{
 
 router.post("/save", async (req, res) => {
 
+    console.log("BODY RECIBIDO:");
+    console.log(req.body);
+
     try {
 
         const artwork = await Artwork.create(req.body);
 
+        console.log("GUARDADO:", artwork);
+
         res.status(201).json(artwork);
 
     } catch (error) {
+
+        console.error("ERROR:", error);
 
         res.status(500).json({
             error: error.message
