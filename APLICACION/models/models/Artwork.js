20e3eb5d676745a7751bf18a0b6bf4037ffb7d94
Laura Ortiz <131725116+Luciaortz@users.noreply.github.com>
2026-05-28 22:29:26 -0500

    se modifico el codigo del arte

---
+++ APLICACION/models/models/Artwork.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ArtworkSchema = new mongoose.Schema({
+  nombreObra: {
+    type: String,
+    required: true
+  },
+
+  nombreUsuario: {
+    type: String,
+    required: true
+  },
+
+  imagen: {
+    type: String,
+    required: true
+  },
+
+  fechaCreacion: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Artwork", ArtworkSchema);
